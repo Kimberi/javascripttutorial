1a93013d7900508187d87f89294f841b35947e0a
kimberi <berikim61@gmail.com>
2023-08-11 22:01:57 +0100

    changed kim.html

---
+++ app.js
@@ -0,0 +1,45 @@
+console.log("hello world");
+var number1 = 12;
+var number2 = 5;
+var number1= number1+ number2;
+number1--
+document.write(number1);
+let myage= 12;
+console.log(myage);
+//control statments
+let country="cameroon";
+let female =10;
+if(female>=20 && country=="cameroon"){
+console.log("you can get married");
+}
+else if(female< 20){
+console.log('you are a teenager');
+}
+else{
+ console.log('you cannot')
+}
+var number3 = 30;
+var number4 = 5;
+var number3= number3- number4;
+number3--
+document.write(number3);
+let value = [];
+switch (typeof value) {
+    case 'number':
+        console.log('number')
+        
+        break;
+        case 'string':
+        console.log('string')
+        
+        break;
+        case 'boolean':
+        console.log('boolean')
+        
+        break;
+
+    default:
+        console.log('others')
+        break;
+}
+
